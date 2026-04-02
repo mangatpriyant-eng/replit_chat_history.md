f2f623315f6a8e009d1834f59716292865d678e6
mangatpriyant-eng <mangatpriyant@gmail.com>
2026-04-02 20:26:42 +0000

    unset GITHUB_TOKEN git add player.cpp git commit -m "Add Player class with constructor and move method" git push

--- Sprite.cpp
+++ Sprite.cpp
@@ -13,7 +13,10 @@ struct Sprite {
         visible = true;
     }
 } ; // why the semicolon at the end of the struct definition?
-
+float distanceFromOrigin() {
+   return  x + y;
+    std::cout << distanceFromOrigin() << std::endl;
+    }
 int main() {
     Sprite s;
     s.x = 10.0;
@@ -24,5 +27,9 @@ int main() {
 std::cout << s.visible<< std::endl; 
 s.hide();
 std::cout << s.visible << std::endl;
-
+//float distanceFromOrigin() {
+  //  return s.x + s.y;
+    //std::cout << s.distanceFromOrigin() << std::endl;
+    return 0;
 }
+//}
